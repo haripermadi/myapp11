628248569de1974bc10f9335dc5535d67a5e28da
haripermadi <harinurpermadi@gmail.com>
2020-04-04 10:14:50 +0700

    add helper logic, ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,68 @@
+import './styles.css'
 import React from 'react'
+import { FaPlus, FaEquals } from 'react-icons/fa';
 
+import { sum } from '../helper/index'
 
-const App = () => {
-  return (
-    <div>
-      halooo
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstNum: null,
+      secNum: null,
+      result: 0
+
+    }
+  }
+
+  handleInput = (e, type) => {
+    e.preventDefault()
+    console.log('---input----', e.target.value, type)
+    this.setState({
+      [type]: e.target.value
+    })
+  }
+
+  handleCalculate = () => {
+    let num1 = parseInt(this.state.firstNum)
+    let num2 = parseInt(this.state.secNum)
+    console.log(num1, typeof num1, num2, typeof num2, sum(num1, num2))
+    this.setState({
+      result: sum(num1, num2)
+    })
+  }
+
+  render() {
+    console.log('render------', this.state)
+    return (
+      <div className='container'>
+        <div className="task">
+          <h3 className="title">Task 1 Addition</h3>
+          <div className="container-task">
+            <div className="form-group mx-sm-3 mb-2">
+              <input type="number" className="form-control text-center" placeholder="1" onChange={(e) => this.handleInput(e, 'firstNum')} />
+            </div>
+            <FaPlus className="icon-style" />
+            <div className="form-group mx-sm-3 mb-2">
+              <input type="number" className="form-control text-center" placeholder="1" onChange={(e) => this.handleInput(e, 'secNum')} />
+            </div>
+            <FaEquals className="icon-style" />
+            <span className="result">{this.state.result}</span>
+          </div>
+          <button type="submit" className="btn btn-primary mb-2" onClick={this.handleCalculate}>Calculate</button>
+        </div>
+        <div>
+          <h3>Task 2 Multiple</h3>
+        </div>
+        <div>
+          <h3>Task 3 N Prime Number</h3>
+        </div>
+        <div>
+          <h3>Task 4 N Fibonacci Number</h3>
+        </div>
+      </div>
+    )
+  }
 }
 
 
---
+++ src/helper/index.js
@@ -0,0 +1,60 @@
+// sum
+function sum(num1, num2) {
+  return num1 + num2
+}
+
+// multiple
+function multiple(num1, num2) {
+  return num1 * num2
+}
+
+
+function primeNumber(n) {
+  //check prime number
+  function checkPrime(num) {
+    if (num < 2) {
+      return false
+    } else if (num === 2) {
+      return true
+    } else {
+      for (let j = 2; j < num; j++) {
+        if (num % j === 0) {
+          return false
+        }
+      }
+      return true
+    }
+  }
+  const primeNumberList = []
+  let num = n * 3 - 3
+  for (let i = 2; i <= num; i++) {
+    if (checkPrime(i)) {
+      primeNumberList.push(i)
+    }
+  }
+  return primeNumberList
+}
+
+// first N fibonanci
+function fibonanci(num) {
+  let result = [0, 1]
+
+  for (let i = 2; i < num; i++) {
+    // console.log(i,'---',i-1,i-2)
+    result.push(result[i - 1] + result[i - 2])
+  }
+
+  return result
+}
+
+// console.log(sum(1,99))
+// console.log(multiple(1,99))
+// console.log(primeNumber(4)) // 2,3,5,7
+// console.log(fibonanci(5)) // 0,1,1,2
+
+export {
+  sum,
+  multiple,
+  primeNumber,
+  fibonanci
+}
